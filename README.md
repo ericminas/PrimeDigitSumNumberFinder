b5dda652cc41f7be40106eb14c97ecdaf141a779
eric_l1 <s8ermina@stud.uni-saarland.de>
2023-11-09 15:44:53 +0100

    V2: now allows the computation of the addition property

--- README.md
+++ README.md
@@ -5,11 +5,20 @@ A program that finds all numbers up to a given max_value, that fulfill the follo
 
 The number is a string prime-digit-prime-number, if it is less than 100 and or the number is a prime.
 
+An additional property is:
+Whether the digits of the number can be used to get all numbers bigger than the highest digit.
+Example: 23 -> 2,3. 
+
+2 and 3 can be used to create 4 (2+2), 5 (3+2), 6 (3+3), 7(2+2+3), ....
+
+The exact check is whether the digits can create the next 50 numbers. If that is the case, we expect them to be able to create all higher numbers.
+
 ## Usage
-```python finder.py start end primeOnly```
+```python finder.py start end primeOnly additionProp```
 with:
 | argument name | description |
 |---------------|-------------|
 | start         | The first number that should be checked (inclusive) |
 | end           | The last number that should be checked (inclusive) |
 | primeOnly     | Whether only prime numbers should be checked (1, true.lower()) |  
+| additionProp  | Whether the number should have the addition propert (described above) (1, true.lower()) |
--- finder.py
+++ finder.py
@@ -32,12 +32,48 @@ def isDigitSumPrime(digits):
     return isSavedPrime(sum)
 
 
-def checkNumber(number):
+def isNumberReachable(digits, number):
+    # multiple of digit
+    for d in digits:
+        if number % d == 0:
+            return True
+
+    for d1 in digits:
+        for d2 in digits:
+            if (number - d1) < 0:
+                return False
+            if (number - d1) % d2 == 0:
+                return True
+
+    return False
+
+
+def digitsSatisfyAdditionProperty(digits):
+    # get the highest digit
+    highestDigit = max(digits)
+
+    for i in range(highestDigit, highestDigit + 50):
+        if not isNumberReachable(digits, i):
+            return False
+    return True
+
+
+def checkNumber(number, additionProp):
     isPrime = isSavedPrime(number)
     digits = getDigits(number)
     requirements = areDigitsPrime(digits) and isDigitSumPrime(digits)
 
     if requirements:
+        if additionProp:
+            return (
+                number,
+                "prime" if isPrime else "not-prime",
+                "strong" if number < 100 else "not strong",
+                "addition property fulfilled"
+                if digitsSatisfyAdditionProperty(digits)
+                else "addition property not fulfilled",
+            )
+
         return (
             number,
             "prime" if isPrime else "not-prime",
@@ -46,19 +82,19 @@ def checkNumber(number):
 
 
 def printResult(result):
-    result = checkNumber(number)
     if result is not None:
         print(result)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print("Usage: python finder.py start end primeonly")
     else:
         try:
             start = int(sys.argv[1])
             end = int(sys.argv[2])
             primeonly = (sys.argv[3]) == 1 or str(sys.argv[3]).lower() == "true"
+            additionProp = (sys.argv[4]) == 1 or str(sys.argv[4]).lower() == "true"
         except ValueError:
             print(
                 "Please enter valid integer values for start and end and 0/1 for primeonly."
@@ -68,9 +104,11 @@ if __name__ == "__main__":
     print(
         "starting at: ",
         start,
-        "up to: ",
+        "\nup to: ",
         end,
-        "using only prime numbers" if primeonly else "using all numbers",
+        "\nusing only prime numbers" if primeonly else "using all numbers.",
+        "\n"+("" if additionProp else "not ") + "checking addition property.",
+        "\n"
     )
 
     if primeonly:
@@ -80,8 +118,8 @@ if __name__ == "__main__":
             numbers = data["primenumbers"]
             for number in numbers:
                 if start <= number and number <= end:
-                    printResult(checkNumber(number))
+                    printResult(checkNumber(number, additionProp))
 
     else:
         for number in range(start, end):
-            printResult(checkNumber(number))
+            printResult(checkNumber(number, additionProp))
